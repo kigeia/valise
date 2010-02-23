31d991eeb4d0394e137e6a67c92cc3a751e2f274
tcrespin <tcrespin@3af1deee-4ace-4d6f-927a-55d690a2d960>
2010-02-23 08:51:42 +0000

    Passage sous licence GPL 3
    git-svn-id: https://svn.devsesamath.net/svn/sacoche/trunk@42 3af1deee-4ace-4d6f-927a-55d690a2d960

--- _inc/fonction_css_browser_selector.php
+++ _inc/fonction_css_browser_selector.php
@@ -1,19 +1,38 @@
 <?php
 /**
  * @version $Id$
+ * @author Thomas Crespin <thomas.crespin@sesamath.net>
+ * @copyright Thomas Crespin 2010
  * 
  * ****************************************************************************************************
- * SACoche [http://competences.sesamath.net] - Suivi d'Acquisitions de Compétences
- * © Thomas Crespin pour Sésamath [http://www.sesamath.net]
- * Distribution sous licence libre prévue pour l'été 2010.
+ * SACoche <http://competences.sesamath.net> - Suivi d'Acquisitions de Compétences
+ * © Thomas Crespin pour Sésamath <http://www.sesamath.net> - Tous droits réservés.
+ * Logiciel placé sous la licence libre GPL 3 <http://www.rodage.org/gpl-3.0.fr.html>.
  * ****************************************************************************************************
+ * 
+ * Ce fichier est une partie de SACoche.
+ * 
+ * SACoche est un logiciel libre ; vous pouvez le redistribuer ou le modifier suivant les termes 
+ * de la “GNU General Public License” telle que publiée par la Free Software Foundation :
+ * soit la version 3 de cette licence, soit (à votre gré) toute version ultérieure.
+ * 
+ * SACoche est distribué dans l’espoir qu’il vous sera utile, mais SANS AUCUNE GARANTIE :
+ * sans même la garantie implicite de COMMERCIALISABILITÉ ni d’ADÉQUATION À UN OBJECTIF PARTICULIER.
+ * Consultez la Licence Générale Publique GNU pour plus de détails.
+ * 
+ * Vous devriez avoir reçu une copie de la Licence Générale Publique GNU avec SACoche ;
+ * si ce n’est pas le cas, consultez : <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+/**
  * PHP CSS Browser Selector v0.0.1
  * @author Bastian Allgeier (http://bastian-allgeier.de)
  * http://bastian-allgeier.de/css_browser_selector
  * License: http://creativecommons.org/licenses/by/2.5/
  * Credits: This is a php port from Rafael Lima's original Javascript CSS Browser Selector: http://rafael.adm.br/css_browser_selector
- * ****************************************************************************************************
  * 
+ * Fonction originale réécrite et modifiée pour SACoche par Thomas Crespin.
  */
 
 function css_browser_selector($UserAgent=null)
--- pages_directeur/fichier_arbre-socle.ajax.php
+++ pages_directeur/fichier_arbre-socle.ajax.php
@@ -2,13 +2,26 @@
 /**
  * @version $Id$
  * @author Thomas Crespin <thomas.crespin@sesamath.net>
- * @copyright Thomas Crespin 2009
+ * @copyright Thomas Crespin 2010
  * 
  * ****************************************************************************************************
- * SACoche [http://competences.sesamath.net] - Suivi d'Acquisitions de Compétences
- * © Thomas Crespin pour Sésamath [http://www.sesamath.net]
- * Distribution sous licence libre prévue pour l'été 2010.
+ * SACoche <http://competences.sesamath.net> - Suivi d'Acquisitions de Compétences
+ * © Thomas Crespin pour Sésamath <http://www.sesamath.net> - Tous droits réservés.
+ * Logiciel placé sous la licence libre GPL 3 <http://www.rodage.org/gpl-3.0.fr.html>.
  * ****************************************************************************************************
+ * 
+ * Ce fichier est une partie de SACoche.
+ * 
+ * SACoche est un logiciel libre ; vous pouvez le redistribuer ou le modifier suivant les termes 
+ * de la “GNU General Public License” telle que publiée par la Free Software Foundation :
+ * soit la version 3 de cette licence, soit (à votre gré) toute version ultérieure.
+ * 
+ * SACoche est distribué dans l’espoir qu’il vous sera utile, mais SANS AUCUNE GARANTIE :
+ * sans même la garantie implicite de COMMERCIALISABILITÉ ni d’ADÉQUATION À UN OBJECTIF PARTICULIER.
+ * Consultez la Licence Générale Publique GNU pour plus de détails.
+ * 
+ * Vous devriez avoir reçu une copie de la Licence Générale Publique GNU avec SACoche ;
+ * si ce n’est pas le cas, consultez : <http://www.gnu.org/licenses/>.
  * 
  */
 
@@ -94,7 +107,7 @@ if($palier_id && $palier_nom)
 	// Finalisation de l'export CSV (archivage dans un fichier zippé)
 	$fnom = 'export_'.$_SESSION['STRUCTURE_ID'].'_'.$_SESSION['USER_ID'].'_arbre-socle_'.$palier_id.'_'.time();
 	$zip = new ZipArchive();
-	if ($zip->open($dossier.$fnom.'.zip', ZIPARCHIVE::CREATE)===TRUE)
+	if ($zip->open($dossier_export.$fnom.'.zip', ZIPARCHIVE::CREATE)===TRUE)
 	{
 		$zip->addFromString($fnom.'.csv',csv($export_csv));
 		$zip->close();
@@ -103,7 +116,7 @@ if($palier_id && $palier_nom)
 	$export_html.= '</div>';
 
 	// Affichage
-	echo'<ul class="puce"><li><a class="lien_ext" href="'.$dossier.$fnom.'.zip">Récupérez l\'arborescence dans un fichier au format CSV.</a></li></ul><p />';
+	echo'<ul class="puce"><li><a class="lien_ext" href="'.$dossier_export.$fnom.'.zip">Récupérez l\'arborescence dans un fichier au format CSV.</a></li></ul><p />';
 	echo $export_html;
 }
 

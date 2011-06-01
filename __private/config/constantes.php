201f576e503e8da6cf1964553e395a2a4eb3df99
Josselin Jacquard <joss@ks2014438.kimsufi.com>
2011-06-01 17:59:54 +0200

    configuration locale du serveur

---
+++ __private/config/constantes.php
@@ -0,0 +1,22 @@
+<?php
+// Informations concernant l'hébergement et son webmestre (n°UAI uniquement pour une installation de type mono-structure)
+define('HEBERGEUR_INSTALLATION'   ,'mono-structure');
+define('HEBERGEUR_DENOMINATION'   ,'Collège Joliot-Curie');
+define('HEBERGEUR_UAI'            ,'0170072L');
+define('HEBERGEUR_ADRESSE_SITE'   ,'www.joliot-curie.fr');
+define('HEBERGEUR_LOGO'           ,'');
+define('HEBERGEUR_CNIL'           ,'0');
+define('WEBMESTRE_NOM'            ,'JACQUARD');
+define('WEBMESTRE_PRENOM'         ,'Josselin');
+define('WEBMESTRE_COURRIEL'       ,'josselin.jacquard@ac-poitiers.fr');
+define('WEBMESTRE_PASSWORD_MD5'   ,'0c80b7fae8c936f067aecd8696b2ffb8');
+define('WEBMESTRE_ERREUR_DATE'    ,'1298384401');
+define('SERVEUR_PROXY_USED'       ,'');
+define('SERVEUR_PROXY_NAME'       ,'');
+define('SERVEUR_PROXY_PORT'       ,'');
+define('SERVEUR_PROXY_TYPE'       ,'');
+define('SERVEUR_PROXY_AUTH_USED'  ,'');
+define('SERVEUR_PROXY_AUTH_METHOD','');
+define('SERVEUR_PROXY_AUTH_USER'  ,'');
+define('SERVEUR_PROXY_AUTH_PASS'  ,'');
+?>
---
+++ __private/mysql/serveur_sacoche_structure.php
@@ -0,0 +1,8 @@
+<?php
+// Paramètres MySQL de la base de données SACoche (installation mono-structure).
+define('SACOCHE_STRUCTURE_BD_HOST','127.0.0.1');	// Nom d'hôte / serveur
+define('SACOCHE_STRUCTURE_BD_PORT','3306');	// Port de connexion
+define('SACOCHE_STRUCTURE_BD_NAME','sacoche');	// Nom de la base
+define('SACOCHE_STRUCTURE_BD_USER','sacoche');	// Nom d'utilisateur
+define('SACOCHE_STRUCTURE_BD_PASS','auie,1');	// Mot de passe
+?>

48609405dfae4f77959936b6e06b04fe261d9f71
Josselin Jacquard <joss@ks2014438.kimsufi.com>
2011-06-02 11:33:27 +0200

    suppression du flash

--- index.php
+++ index.php
@@ -260,11 +260,5 @@ entete();
 		var DUREE_RESTANTE='<?php echo $_SESSION['DUREE_INACTIVITE'] ?>';
 	</script>
 	<?php echo $SCRIPT; ?>
-	<!-- Objet flash pour lire un fichier audio grace au génial lecteur de neolao http://flash-mp3-player.net/ -->
-	<h6><object class="playerpreview" id="myFlash" type="application/x-shockwave-flash" data="./_mp3/player_mp3_js.swf" height="1" width="1">
-		<param name="movie" value="./_mp3/player_mp3_js.swf" />
-		<param name="AllowScriptAccess" value="always" />
-		<param name="FlashVars" value="listener=myListener&amp;interval=500" />
-	</object></h6>
 </body>
 </html>

50533f36e3a5ad3282634f13ef4b0fc78a293261
Josselin Jacquard <josselin.jacquard@gmail.com>
2011-08-02 17:20:35 +0200

    [saml] ajout du paramètre organization dans la requete pour pouvoir initialiser gepi (intervient avant l'initialisation simplesaml)

--- _simplesaml/metadata/saml20-idp-remote.php
+++ _simplesaml/metadata/saml20-idp-remote.php
@@ -13,4 +13,23 @@ $metadata['gepi-idp'] = array(
 	'SingleLogoutService'  => 'https://localhost/gepi/simplesaml/saml2/idp/SingleLogoutService.php',
 	'certFingerprint'      => 'AA:FD:FF:98:48:18:A8:56:73:32:73:8F:33:53:04:8C:36:9B:E6:B2'
 );
+
+//add a preference for the organizationId of the user session
+//pour sacoche c'est dans la requete : id, f_base, ou le cookie
+$organization = '';
+if (isset($_REQUEST['id'])) {
+	$organization= $_REQUEST['id'];
+} else if (isset($_REQUEST['f_base'])) {
+	$organization= $_REQUEST['f_base'];
+} else {
+	$path = dirname(dirname(dirname(__FILE__)));
+	require_once("$path/__private/config/constantes.php");
+	if (isset($_COOKIE[COOKIE_STRUCTURE])) {
+		$organization= $_COOKIE[COOKIE_STRUCTURE];
+	}
+}
+if ($organization != '0' && $organization != '') {
+	$metadata['gepi-idp']['SingleSignOnService'] .= '?organization='.$organization;
+}
+
 ?>

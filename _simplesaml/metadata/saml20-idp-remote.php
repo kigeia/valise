fdcc3cb63a5f23aaa4672dd42121c8cfecc4bc03
Josselin Jacquard <josselin.jacquard@gmail.com>
2011-08-11 21:57:15 +0200

    [saml] Utilisation des paramètre en base de donnée pour la configuration de l'idp

--- _simplesaml/metadata/saml20-idp-remote.php
+++ _simplesaml/metadata/saml20-idp-remote.php
@@ -14,23 +14,21 @@ $metadata['gepi-idp'] = array(
 	'certFingerprint'      => 'AA:FD:FF:98:48:18:A8:56:73:32:73:8F:33:53:04:8C:36:9B:E6:B2'
 );
 
-//add a preference for the organizationId of the user session
-//pour sacoche c'est dans la requete : id, f_base, ou le cookie
-$organization = '';
-if (isset($_REQUEST['id'])) {
-	$organization= $_REQUEST['id'];
-} else if (isset($_REQUEST['f_base'])) {
-	$organization= $_REQUEST['f_base'];
-} else {
-	$path = dirname(dirname(dirname(__FILE__)));
-	define('SACoche','index'); //inutile ici mais obligatoire pour l'include suivant
-	require_once("$path/_inc/constantes.php");
-	if (isset($_COOKIE[COOKIE_STRUCTURE])) {
-		$organization= $_COOKIE[COOKIE_STRUCTURE];
-	}
+$path = dirname(dirname(dirname(__FILE__)));
+require_once("$path/__private/config/constantes.php");
+require_once("$path/__private/mysql/serveur_sacoche_structure.php");
+require_once("$path/_inc/class.DB.config.sacoche_structure.php");
+require_once("$path/_inc/fonction_requetes_structure.php");
+require_once("$path/_lib/DB/DB.class.php");
+
+$DB_TAB = DB_STRUCTURE_lister_parametres('"gepi_url","gepi_rne","gepi_certificat_empreinte"');
+foreach($DB_TAB as $DB_ROW)
+{
+	${$DB_ROW['parametre_nom']} = $DB_ROW['parametre_valeur'];
 }
-if ($organization != '0' && $organization != '') {
-	$metadata['gepi-idp']['SingleSignOnService'] .= '?organization='.$organization;
-}
+
+$metadata['gepi-idp']['SingleSignOnService'] = $gepi_url.'/lib/simplesaml/www/saml2/idp/SSOService.php'.'?rne='.$gepi_rne; /* issu de SACoche */
+$metadata['gepi-idp']['SingleLogoutService'] = 	$gepi_url.'/lib/simplesaml/www/saml2/idp/SingleLogoutService.php'.'?rne='.$gepi_rne; /* issu de SACoche */
+$metadata['gepi-idp']['certFingerprint'] = $gepi_certificat_empreinte;
 
 ?>

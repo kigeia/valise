cce04c44533b456da67402d3e6b2e65bbfc51f26
Josselin Jacquard <josselin.jacquard@gmail.com>
2011-12-22 15:53:43 +0100

    Correction bug mise à jour base

--- _sql/requetes_structure_maj_base.php
+++ _sql/requetes_structure_maj_base.php
@@ -1270,10 +1270,9 @@ public function DB_maj_base($version_actuelle)
 	//	MAJ 2011-10-02 => 2011-10-09
 	//	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	if($version_actuelle=='2011-10-02')
+	if($version_actuelle=='2011-10-02' || $version_actuelle=='xx')
 	{
-		if($version_actuelle==DB_STRUCTURE_MAJ_BASE::DB_version_base()
-			|| 'xx'==DB_STRUCTURE_MAJ_BASE::DB_version_base())
+		if($version_actuelle==DB_STRUCTURE_MAJ_BASE::DB_version_base())
 		{
 			$version_actuelle = '2011-10-09';
 			DB::query(SACOCHE_STRUCTURE_BD_NAME , 'UPDATE sacoche_parametre SET parametre_valeur="'.$version_actuelle.'" WHERE parametre_nom="version_base"' );
